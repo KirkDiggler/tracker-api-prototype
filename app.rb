90f0fe3231f579801bb84896f75d0a21a85f6799
Kirk Gilmore <kirklgilmore@gmail.com>
2019-01-16 10:52:58 -0800

    typo in skipping application controller for app requires

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 # TODO: Investigate better way to get the application controller loaded first
 require_relative 'controllers/application_controller'
-Dir[File.join(File.dirname(__FILE__), 'controllers', '**', '*.rb')].each {|file| require file unless file == 'controllers/application_controller' }
+Dir[File.join(File.dirname(__FILE__), 'controllers', '**', '*.rb')].each {|file| puts file unless file == './controllers/application_controller.rb' }
 
 class TrackerApi < Sinatra::Base
     use DynamicDataController
